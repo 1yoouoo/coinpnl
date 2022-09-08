14992f07cdccd9d498b9e7839f633ef0f16e0073
Blanc <1yoouoo@gmail.com>
2022-09-08 12:02:38 +0900

    add

--- src/pages/editor/editorPage.js
+++ src/pages/editor/editorPage.js
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
 const EditorPage = () => {
-  return <div>editor</div>;
+  // 1. list loop -> <div>
+  // 2. add item
+  const itemList = [
+    {
+      id: "1",
+      desc: "asd",
+    },
+    {
+      id: "2",
+      desc: "asd",
+    },
+    {
+      id: "3",
+      desc: "asd",
+    },
+    {
+      id: "4",
+      desc: "asd",
+    },
+  ];
+  const [list, setList] = useState(itemList);
+  // const list = List<Item>()
+  const addsomething = () => {
+    // add item
+    // list.add(item)
+  };
+  return (
+    <div>
+      <div>screenshot</div>
+
+      <div>
+        {list.map((item) => {
+          return <div key={item.id}>item = {item.desc}</div>;
+        })}
+
+        <div onClick={addsomething}>add</div>
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;
